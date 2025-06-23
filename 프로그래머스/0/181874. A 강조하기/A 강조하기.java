8a8641811e7e261ffad704a82b35743cb4f37cbc
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2025-06-23 17:04:35 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder(myString.toLowerCase());
+        
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == 'a') {
+                sb.setCharAt(i, 'A');
+            }
+        }
+        
+        return sb.toString();
+    }
+}

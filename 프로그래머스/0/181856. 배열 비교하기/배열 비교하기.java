b5b75048bf8c99c347f3df82e484001fc7b4247b
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2025-07-07 16:45:04 +0900

    [level 0] Title: 배열 비교하기, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        
+        if (arr1.length > arr2.length) {
+            return 1;
+        } else if (arr1.length == arr2.length) {
+            int a1 = 0;
+            int a2 = 0;
+            for (int i = 0; i < arr1.length; i++) {
+                a1+=arr1[i];
+            }
+            for (int i = 0; i< arr2.length; i++) {
+                a2+=arr2[i];
+            }
+            if (a1 > a2) {
+                return 1;
+            } else if (a1 == a2) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        
+        
+        return -1;
+    }
+}

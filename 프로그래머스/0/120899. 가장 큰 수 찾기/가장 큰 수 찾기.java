ed1501a8c3f7e2a48fe45cf4d2f473172f4d0822
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2026-06-30 16:29:09 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        int temp = 0;
+        int index = 0;
+        
+        for (int i = 0; i< array.length; i++) {
+            if (temp < array[i]) {
+                temp = array[i];
+                index = i;
+            }
+        }
+        
+        answer[0] = temp;
+        answer[1] = index;
+        
+        return answer;
+    }
+}
